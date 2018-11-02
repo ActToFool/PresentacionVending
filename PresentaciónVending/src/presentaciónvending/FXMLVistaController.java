dd49ba216adae8d3c5fdbb3d346bc8f991986098
Emanuel Álvarez <Emanuel lvarez@10.155.21.223>
2018-11-02 13:03:58 -0500

    pruebas

--- PresentaciónVending/src/presentaciónvending/FXMLVistaController.java
+++ PresentaciónVending/src/presentaciónvending/FXMLVistaController.java
@@ -6,12 +6,19 @@
 package presentaciónvending;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  *
@@ -19,14 +26,43 @@ import javafx.scene.control.Label;
  */
 public class FXMLVistaController implements Initializable {
     
+    private List<String> productoscreados = new ArrayList<>();
+    
     @FXML
     private Label label;
     @FXML
-    private Button button;
+    private Button btnNuevaVenta;
     @FXML
-    private Button ptooooo;
+    private Label lblFecha;
+    @FXML
+    private Button btnAgregarLinea;
+    @FXML
+    private Button btnEliminarLinea;
+    @FXML
+    private Button btnMonedaMil;
+    @FXML
+    private Button btnMonedaQui;
+    @FXML
+    private Button btnTerminarVenta;
+    @FXML
+    private Button btnGenerarReporte;
+    @FXML
+    private ComboBox<String> ListProductos;
     
+    private ObservableList<String> productos = FXCollections.observableArrayList();
     @FXML
+    private Label lblMostrar;
+    @FXML
+    private TableView<String> tblLineasVenta;
+    @FXML
+    private TableColumn<String, String> colProducto;
+    @FXML
+    private TableColumn<String, String> colCantidad;
+    @FXML
+    private TableColumn<String, String> colPrecio;
+    @FXML
+    private TableColumn<String, String> ColSubTotal;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -35,6 +71,24 @@ public class FXMLVistaController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        llenarProductos();
+        configureComboProductos();
+    }  
+    
+    private void llenarProductos(){
+        this.productoscreados.add("Gaseosa");
+        this.productoscreados.add("Hamburguesa");
+        this.productoscreados.add("Pizza");
+        this.productoscreados.add("Almuerzo");
+        
+        productos.setAll(this.productoscreados);
+        ListProductos.setItems(productos);
+        
+    }
+    
+    @FXML
+    public void configureComboProductos(){
+        this.lblMostrar.setText(ListProductos.getValue());
+    }
     
 }
